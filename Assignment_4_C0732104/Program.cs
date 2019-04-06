9042ded49b4c28397e67ad6b4ced006202807b0a
visualstudio <visualstudio@122-PC20>
2019-04-05 21:52:18 -0400

    Section C

--- Assignment_4_C0732104/Program.cs
+++ Assignment_4_C0732104/Program.cs
@@ -1,4 +1,4 @@
-﻿//Section B
+﻿//Section C
 
 
 using System;
@@ -14,20 +14,16 @@ namespace Assignment_4_C0732104
     class Program
     {
         ArrayList Beowulf;
-         static void Main(string[] args)
+        private int countletters;
+
+        static void Main(string[] args)
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
-
-
-            p.ReadTextFiles();
-
+            p.Wordfinder();
             Console.ReadLine();
-
         }
-        public void Run()
-
-            { this.ReadTextFiles(); }
+        public void Run() { this.ReadTextFiles(); }
         public void ReadTextFiles()
         {
             // Read file using StreamReader. Reads file line by line
@@ -35,34 +31,47 @@ namespace Assignment_4_C0732104
             {
                 int counter = 0;
                 string ln;
-
                 while ((ln = file.ReadLine()) != null)
                 {
-
                     Console.WriteLine(ln);
                     Beowulf.Add(ln);
+                    counter++;
+
                 }
                 file.Close();
                 counter = File.ReadLines("U:/Users/732104/Assignment_4_C0732104/Beowulf.txt").Count();
-                Console.WriteLine($"File has {counter } lines.");
-                Console.WriteLine($"File has {counter * 10} Words.");
+                Console.WriteLine($"File has {counter} lines.");
+                Console.WriteLine($"File has {counter * 10} words.");
+
 
             }
         }
-        
-            public int FindNumberOfBlankSpaces(string line)
+        public void Wordfinder()
+        {
+            int f = 0;
+            foreach (var line in File.ReadAllLines("U:/Users/732104/Assignment_4_C0732104/Beowulf.txt"))
             {
-               // https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
-                int countletters = 0;
-                int countSpaces = 0;
-
-                foreach (char c in line)
+                if (line.Contains("sea") && line.Contains("fare"))
                 {
-                    if (char.IsLetter(c)) { countletters++; }
-                    if (char.IsWhiteSpace(c)) { countSpaces++; }
+                    f++;
                 }
-                return countSpaces;
             }
+            Console.WriteLine(f);
+        }
+
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            // hhtp://stackovrflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
+           
+            int countSpaces = 0;
+            foreach (char c in line)
+
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+
+            }
+            return countSpaces;
         }
     }
-
+}
